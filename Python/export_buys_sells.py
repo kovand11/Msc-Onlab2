387ab6b5fa7c4671d63d0f38efc246e66c60fa1a
András Kovács <kovand11@gmail.com>
2016-05-17 16:02:43 +0200

    fixed script, gitignore for csv in python folder

--- Python/export_buys_sells.py
+++ Python/export_buys_sells.py
@@ -21,6 +21,7 @@ def process_range(q,path,beg,end,ex):
     for i in range(len(dates)):
         if beg <= dates[i] < end:
             #open the tind qind partially
+            date = dates[i]#dbg
             dateletter = str(dates[i])[2:8] + str(disk[i])[2]
             tibeg = int(tb[i])
             tiend = int(te[i])
@@ -30,7 +31,9 @@ def process_range(q,path,beg,end,ex):
             q.sync('ctx_table:flip `SYMBOL`TDATE`TBEGREC`TENDREC!("siii";10 4 4 4) 1: (`:%sT%s.IDX,%d,%d)' % (path,dateletter,(tibeg-1)*index_len,(tiend-tibeg)*index_len))
             q.sync('cqx_table:flip `SYMBOL`QDATE`QBEGREC`QENDREC!("siii";10 4 4 4) 1: (`:%sQ%s.IDX,%d,%d)' % (path,dateletter,(qibeg-1)*index_len,(qiend-qibeg)*index_len))
             symbol_index = q.sync('select SYMBOL,TBEG:TBEGREC,TEND:TENDREC,QBEG:QBEGREC,QEND:QENDREC from ctx_table ij (`SYMBOL xkey cqx_table)')
-            csvfile = open(str(dates[i])+'.csv', 'w')
+            date_str = str(dates[i])
+            date_str = date_str[2:len(date_str)-1]
+            csvfile = open(str(date_str+'.csv'), 'w')
             for i in range(0, len(symbol_index)):
                 t_rec_len = 29
                 q_rec_len = 39
@@ -39,14 +42,17 @@ def process_range(q,path,beg,end,ex):
                 tend = symbol_index[i][2]
                 qbeg = symbol_index[i][3]
                 qend = symbol_index[i][4]
-                print("%d/%d procesing: %s" % (i, len(symbol_index), symb))
+                print("%s: %d/%d procesing: %s" % (date_str,i, len(symbol_index), symb))
                 # loading the tables server side
+                if symb == numpy.string_('PDCO'):
+                    print('ctb_table:flip `TTIM`PRICE`SIZ`TSEQ`G127`CORR`COND`EX!("ijiihhsc";4 8 4 4 2 2 4 1) 1: (`:%sT%s.BIN,%s,%s)' % (
+                    path,dateletter,(tbeg - 1) * t_rec_len, (tend - tbeg) * t_rec_len))
                 q.sync(
-                    'ctb_table:flip `TTIM`PRICE`SIZ`TSEQ`G127`CORR`COND`EX!("ijiihhsc";4 8 4 4 2 2 4 1) 1: (`:/q/data/T200405A.BIN,%s,%s)' % (
-                    (tbeg - 1) * t_rec_len, (tend - tbeg) * t_rec_len))
+                    'ctb_table:flip `TTIM`PRICE`SIZ`TSEQ`G127`CORR`COND`EX!("ijiihhsc";4 8 4 4 2 2 4 1) 1: (`:%sT%s.BIN,%s,%s)' % (
+                    path,dateletter,(tbeg - 1) * t_rec_len, (tend - tbeg) * t_rec_len))
                 q.sync(
-                    'ctq_table:flip `QTIM`BID`OFR`QSEQ`BIDSIZE`OFRSIZ`MODE`EX`MMID!("ijjiiihcs";4 8 8 4 4 4 2 1 4) 1: (`:/q/data/Q200405A.BIN,%s,%s)' % (
-                    (qbeg - 1) * q_rec_len, (qend - qbeg) * q_rec_len))
+                    'ctq_table:flip `QTIM`BID`OFR`QSEQ`BIDSIZE`OFRSIZ`MODE`EX`MMID!("ijjiiihcs";4 8 8 4 4 4 2 1 4) 1: (`:%sQ%s.BIN,%s,%s)' % (
+                    path,dateletter,(qbeg - 1) * q_rec_len, (qend - qbeg) * q_rec_len))
                 q.sync('t_sym_ex:`TTIM xasc select TTIM,PRICE from ctb_table where EX="%s"' % ex)
                 q.sync('q_sym_ex:`QTIM xasc select QTIM,BID,OFR from ctq_table where EX="%s"' % ex)
                 t_sym_ex = q.sync('flip t_sym_ex')
@@ -64,40 +70,41 @@ def process_range(q,path,beg,end,ex):
                 lee_ready_class = [None] * len(ttim)
                 for j in range(0, len(ttim)):
                     ind = bisect.bisect_left(qtim, ttim[j] - 5)  # best bid and ofr from 5 sec before
-                    lee_ready_class[j] = 'green'
+                    lee_ready_class[j] = 0
                     if ind != 0:
                         lbid[j] = bid[ind - 1]
                         lofr[j] = ofr[ind - 1]
                         avg = (lbid[j] + lofr[j]) / 2
                         if price[j] > avg:
-                            lee_ready_class[j] = 'red'
+                            lee_ready_class[j] = 1
                         elif price[j] < avg:
-                            lee_ready_class[j] = 'blue'
+                            lee_ready_class[j] = -1
                         else:
-                            if price[j] > price[j - 1]:
-                                lee_ready_class[j] = 'red'
-                            elif price[j] < price[j - 1]:
-                                lee_ready_class[j] = 'blue'
+                            for k in  range(1,j):
+                                if price[j] > price[j - k]:
+                                    lee_ready_class[j] = 1
+                                elif price[j] < price[j - k]:
+                                    lee_ready_class[j] = -1
                 #output
                 out_sym = str(symb)
-                out_sell = lee_ready_class.count('blue');
-                out_buy = lee_ready_class.count('red');
-                csvfile.write(out_sym+','+out_buy+','+out_sell+'\n')
+                out_sym = out_sym[2:len(out_sym)-1]
+                out_sell = str(lee_ready_class.count(-1));
+                out_buy = str(lee_ready_class.count(1));
+                out_unclass = str(lee_ready_class.count(0));
+                csvfile.write(out_sym+','+out_buy+','+out_sell+','+out_unclass+'\n')
             csvfile.close()
 
     return
 
 if __name__ == '__main__':
-    qcon = qconnection.QConnection(host='localhost', port=5000) #bme-host = f1.finance.bme.hu
+    qcon = qconnection.QConnection(host='f1.finance.bme.hu', port=5001) #bme-host = f1.finance.bme.hu
     qcon.open()
     print(qcon)
     print('IPC version: %s. Is connected: %s' % (qcon.protocol_version, qcon.is_connected()))
     start_time = time.time()
-    process_range(qcon,'/storage/share/',numpy.string_('20050101'),numpy.string_('20060101'),'A')
+    process_range(qcon,'/storage/share/',numpy.string_('20040501'),numpy.string_('20040601'),'A')
     elapsed = time.time() - start_time;
     print('classify_trades(qcon,\'A\') took: %d' % elapsed)
     qcon.close()
-
-    plot_test()
 
 #set
